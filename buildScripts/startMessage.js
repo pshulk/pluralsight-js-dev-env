ed53e8137f229ccada1c7f36ac702d7325b5f4d8
Paul Shulkin <psh@alpha-solutions.us>
2020-12-20 11:28:00 -0500

    up to chapter 10

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.green('starting app in dev mode')); // eslint-disable-line no-console
+
+
---
+++ buildScripts/testSetup.js
@@ -0,0 +1,7 @@
+// this file is not transpiled so must use CommonJS and ES5
+
+// register babel to transpile before our tests run
+require('babel-register')();
+
+// disable webpack features that Mocha doesn't understand
+require.extensions['.css']=function(){};
